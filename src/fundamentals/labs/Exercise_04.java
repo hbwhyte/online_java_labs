16b1dae2f75cfe8ac27cd1004772fe67e7357bca
hbwhyte <heather.b.whyte@gmail.com>
2018-02-03 16:50:05 -0700

    Complete cylinder calculations

--- src/fundamentals/labs/Exercise_04.java
+++ src/fundamentals/labs/Exercise_04.java
@@ -3,7 +3,7 @@ package fundamentals.labs;
 /**
  * Fundamentals Exercise 4: Area and Perimeter
  *
- *      Write the necessary code to calculate the area and perimeter of a cylinder
+ *      Write the necessary code to calculate the volume and surface area of a cylinder
  *      with a radius of 3.14 and a height of 5. Print out the result.
  *
  */
@@ -12,9 +12,12 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int radius;
+        float radius = 3.14F;
+        int height = 5;
+        double volume = Math.PI * radius * radius * height;
+        double sArea = (2 * Math.PI * radius * height) + (2 * Math.PI * radius * radius);
 
-
-
+        System.out.println("The volume is " + volume);
+        System.out.println("The surface area is " + sArea);
     }
 }
