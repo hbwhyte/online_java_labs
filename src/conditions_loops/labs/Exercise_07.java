23c8a241693e05a2fa252a8c2d687c560a36e23b
hbwhyte <heather.b.whyte@gmail.com>
2018-02-12 18:06:13 -0700

    Update conditions loops exercise

--- src/conditions_loops/labs/Exercise_07.java
+++ src/conditions_loops/labs/Exercise_07.java
@@ -19,17 +19,16 @@ public class Exercise_07 {
 
         // for (int i = 0; i <= length; i++)  { // I know this is a for loop. How to change to while?
         int i = 0;
-        while (i <= word.length()) {
+        while (i < word.length()) {
             char vowel = word.charAt(i);
-            // if (vowel == ('a' | 'e' | 'o' | 'i' | 'u'))
+            // if (vowel == ('a' | 'e' | 'o' | 'i' | 'u')) {
             if (vowel == ('a' | 'e')) {
                 System.out.println("The first vowel in " + word + " is " + vowel);
                 break;
             }
             i++;
         }
-        // It freaks out if I have too many or statements on ln 22
-        // "Exception in thread "main" java.lang.StringIndexOutOfBoundsException: String index out of range:"
+        // How to use logical operators on char?
 
     }
 }
