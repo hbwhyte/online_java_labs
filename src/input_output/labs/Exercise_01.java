ffaf287a00f1fa3e3c55e3ffd5df0dfb5cbe7363
hbwhyte <heather.b.whyte@gmail.com>
2018-03-01 22:34:54 -0800

    Input Output homework update and complete

--- src/input_output/labs/Exercise_01.java
+++ src/input_output/labs/Exercise_01.java
@@ -9,3 +9,50 @@ package input_output.labs;
  *
  */
 
+
+import java.io.*;
+
+class BufferStream {
+
+    public static void main(String[] args) {
+
+        // Declare Input/Output Streams
+        BufferedInputStream bin = null;
+        BufferedOutputStream bout = null;
+
+//        // Make sure both files are specified
+//        if (args.length != 2) {
+//            System.out.println("Usage: CopyFile from to");
+//            return;
+//        }
+
+        try {
+            // Initialize
+            int b;
+            bin = new BufferedInputStream(new FileInputStream("/Users/hbwhyte/CodingNomads/online_java_labs/src/input_output/lab_files/sampleFile.txt"));
+            bout = new BufferedOutputStream(new FileOutputStream("/Users/hbwhyte/CodingNomads/online_java_labs/src/input_output/lab_files/sampleFile2.txt"));
+
+            // While loop
+            while ((b = bin.read()) != -1) {
+                bout.write(b);
+            }
+
+            // Catch exceptions
+        } catch (IOException e) {
+            System.out.println("IO Exception caught");
+
+            // Close file connections
+        } finally {
+            try {
+                if (bin != null) {
+                    bin.close();
+                }
+                if (bout != null) {
+                    bout.close();
+                }
+            } catch (IOException ioe) {
+                System.out.println("IOException caught - No connections to close.");
+            }
+        }
+    }
+}
--- src/input_output/labs/Exercise_02.java
+++ src/input_output/labs/Exercise_02.java
@@ -12,3 +12,50 @@ package input_output.labs;
  *
  */
 
+import java.io.*;
+
+class EncryptedStream {
+    public static void main(String[] args) {
+
+        // Declare Streams
+        BufferedReader in = null;
+        BufferedWriter out = null;
+
+        // Initialize with try
+        try {
+
+            int c;
+            in = new BufferedReader(new FileReader("/Users/hbwhyte/CodingNomads/online_java_labs/src/input_output/lab_files/sampleFile.txt"));
+            out = new BufferedWriter(new FileWriter("/Users/hbwhyte/CodingNomads/online_java_labs/src/input_output/lab_files/sampleFileEncrypted.txt"));
+
+            // While loop - change a to - and e to ~
+            while ((c = in.read()) != -1) {
+                if (c == 'a') {
+                    out.write('-');
+                } else if (c == 'e') {
+                    out.write('~');
+                } else {
+                    out.write(c);
+                }
+            }
+
+            // Catch exceptions
+        } catch (IOException e) {
+            System.out.println("IO Exception caught");
+
+            // Close file connections
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException ioe) {
+                System.out.println("IOException caught - No connections to close.");
+            }
+        }
+    }
+
+}
